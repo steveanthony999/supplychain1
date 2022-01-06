f1f819515fe49caa5a57dc397296c2e15d7f1533
Steven Woodward <steveanthony999@gmail.com>
2022-01-05 22:11:08 -0800

    setting up router and auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,7 +11,10 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
+// Init firebase
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
 
-export default firebase;
+// Init Services
+const projectAuth = firebase.auth();
+
+export { projectAuth };
---
+++ src/views/master/Master.js
@@ -0,0 +1,12 @@
+import './Master.css';
+
+const Master = () => {
+  return (
+    <div className='Master'>
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+
+export default Master;
